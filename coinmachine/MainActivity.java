a227d33ff792f6fcc8e972737b8b3e4461806af7
raymond <raymond@oa.cn>
2018-07-31 18:32:37 +0800

    oa

--- coinmachine/MainActivity.java
+++ coinmachine/MainActivity.java
@@ -261,7 +261,7 @@ public class MainActivity extends Activity {
                            String url = mUrls[rd.nextInt(mUrls.length-1)];
                            webView.loadUrl(url);
                            try {
-                               sleep(rd3.nextInt(20000)+10000);
+                               sleep(rd3.nextInt(300*000)+20*000);
                            } catch (InterruptedException e) {
                                e.printStackTrace();
                            }
